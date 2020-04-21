b922cb99b30598075668aa0827259eed4b7c1a3a
RajatSablok <rajat.main06@gmail.com>
2020-04-22 00:55:24 +0530

    user signup login added

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const app = express ();
 
 const listingRoutes = require('./api/routes/listings');
 const cartRoutes = require('./api/routes/carts');
+const userRoutes = require('./api/routes/users');
 
-const PORT = process.env.PORT || 3000;
 const dbPass = process.env.MONGO_ATLAS_PW
 const dbURI = 'mongodb+srv://RajatSablok:'+dbPass+'@cluster0-w6myw.mongodb.net/test?retryWrites=true&w=majority';
 
@@ -24,14 +24,42 @@ app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Allow CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+// Routes that handle requests
 app.use('/listings', listingRoutes);
 app.use('/cart', cartRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
   next(error);
 });
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+        message: error.message
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
     console.log(`Listening on port ${PORT}`)
